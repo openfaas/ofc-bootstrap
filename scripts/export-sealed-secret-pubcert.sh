099b01e59aa81b6d731023a9f14e46ea52bec3da
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-12-19 10:48:07 +0000

    Update SealedSecrets not to be rate-limited
    Applies logic from faas-cli install script to prevent being
    blocked by rate-limiting from GitHub (50 per hour max)
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- scripts/export-sealed-secret-pubcert.sh
+++ scripts/export-sealed-secret-pubcert.sh
@@ -5,7 +5,9 @@ then
     GOOS=$(go env GOOS)
     GOARCH=$(go env GOARCH)
 
-    release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
+    release=$(curl -sI https://github.com/bitnami-labs/sealed-secrets/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
+
+#    release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
     echo "SealedSecrets release: $release"
 
     curl -sLSf https://github.com/bitnami/sealed-secrets/releases/download/$release/kubeseal-$GOOS-$GOARCH > kubeseal && \
--- scripts/install-sealedsecrets.sh
+++ scripts/install-sealedsecrets.sh
@@ -3,7 +3,8 @@
 GOOS=$(go env GOOS)
 GOARCH=$(go env GOARCH)
 
-release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
+release=$(curl -sI https://github.com/bitnami-labs/sealed-secrets/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
+#release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
 
 echo "SealedSecrets release: $release"
 
