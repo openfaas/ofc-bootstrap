734ad8d758edc89ce3d48d5681cef421b56315fe
Alex Ellis <alexellis2@gmail.com>
2018-12-20 22:30:38 +0000

    Have minio quote secrets
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- scripts/install-minio.sh
+++ scripts/install-minio.sh
@@ -4,5 +4,5 @@ export ACCESS_KEY=$(kubectl get secret -n openfaas-fn s3-access-key -o jsonpath=
 export SECRET_KEY=$(kubectl get secret -n openfaas-fn s3-secret-key -o jsonpath='{.data.s3-secret-key}'| base64 --decode)
 
 helm install --name cloud-minio --namespace openfaas \
-   --set accessKey=$ACCESS_KEY,secretKey=$SECRET_KEY,replicas=1,persistence.enabled=false,service.port=9000,service.type=NodePort \
+   --set accessKey="$ACCESS_KEY",secretKey="$SECRET_KEY",replicas=1,persistence.enabled=false,service.port=9000,service.type=NodePort \
   stable/minio
