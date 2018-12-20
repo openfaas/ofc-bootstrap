0ec2b3410a867efdec1a89ffa1c3f4643ce06163
Alex Ellis <alexellis2@gmail.com>
2018-12-20 22:25:04 +0000

    Remove minio during reset
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-helm delete --purge cert-manager nginxingress openfaas
+helm delete --purge cert-manager nginxingress openfaas cloud-minio
 kubectl delete ns openfaas openfaas-fn
 kubectl delete crd -n kube-system --all
 kubectl delete deploy/sealed-secrets-controller -n kube-system
 kubectl delete deploy/tiller-deploy -n kube-system
 kubectl delete sa/tiller -n kube-system
 kubectl delete clusterrolebinding/tiller -n kube-system
-
