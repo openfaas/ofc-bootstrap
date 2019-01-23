65a2d6d7881f097308646245df8c13b5df012b4f
Bart Smykla <bsmykla@vmware.com>
2019-01-23 10:57:22 +0000

    Removing only necessary CRDs instead of all from kube-system
    Because helm is deleting CRDS from cert-manager the only ones
    which we need to remove during resetting process
    is sealedsecrets.bitnami.coms
    
    Signed-off-by: Bart Smykla <bsmykla@vmware.com>

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,7 +2,7 @@
 
 helm delete --purge cert-manager nginxingress openfaas cloud-minio
 kubectl delete ns openfaas openfaas-fn
-kubectl delete crd -n kube-system --all
+kubectl delete crd sealedsecrets.bitnami.com
 kubectl delete deploy/sealed-secrets-controller -n kube-system
 kubectl delete deploy/tiller-deploy -n kube-system
 kubectl delete sa/tiller -n kube-system
