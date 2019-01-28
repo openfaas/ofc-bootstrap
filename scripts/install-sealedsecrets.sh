4c4e407799c1421e8f4d0d81f8a06da9c6d3d9f9
Ivana Yovcheva <iyovcheva@vmware.com>
2019-01-28 17:12:10 +0200

    Install SealedSecrets from chart
    SealedSecrets are now installed with helm chart that is already
    available
    
    Signed-off-by: Ivana Yovcheva <iyovcheva@vmware.com>

--- scripts/install-sealedsecrets.sh
+++ scripts/install-sealedsecrets.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-GOOS=$(go env GOOS)
-GOARCH=$(go env GOARCH)
-
 release=$(curl -sI https://github.com/bitnami-labs/sealed-secrets/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
 #release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
 
 echo "SealedSecrets release: $release"
 
-kubectl create -f https://github.com/bitnami-labs/sealed-secrets/releases/download/$release/sealedsecret-crd.yaml
+helm del --purge ofc-bootstrap
+kubectl delete customresourcedefinition sealedsecrets.bitnami.com
 
-kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/$release/controller.yaml
+helm install --namespace kube-system --name ofc-bootstrap stable/sealed-secrets
