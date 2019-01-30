c77ef3a462c4fa8a0abc5755d667ae069878e2de
Alex Ellis <alexellis2@gmail.com>
2019-01-30 20:17:53 +0000

    Install cert-manager in its own namespace
    - installs cert-manager in "cert-manager" namespace
    - labels cert-manager namespace to prevent "chicken and egg"
    scenario details in upgrade notes around webhook validation
    
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- scripts/install-cert-manager.sh
+++ scripts/install-cert-manager.sh
@@ -3,8 +3,11 @@
 kubectl apply \
     -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
 
+kubectl create namespace cert-manager
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+
 helm install \
     --name cert-manager \
-    --namespace kube-system \
+    --namespace cert-manager \
     --version v0.6.0 \
     stable/cert-manager
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,13 +2,14 @@
 
 helm delete --purge cert-manager nginxingress openfaas cloud-minio
 
-kubectl delete certificates --all  -n openfaas
+kubectl delete certificates --all -n openfaas
+
 kubectl delete ns openfaas openfaas-fn
+kubectl delete ns cert-manager
 
 kubectl delete crd sealedsecrets.bitnami.com
 kubectl delete \
     -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
-
 kubectl delete deploy/sealed-secrets-controller -n kube-system
 kubectl delete deploy/tiller-deploy -n kube-system
 kubectl delete sa/tiller -n kube-system
