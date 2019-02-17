1692fad8c03476744620dd5926630d966e20ed77
Alex Ellis <alexellis2@gmail.com>
2019-02-17 08:46:42 +0000

    Correct ci script
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- hack/ci.sh
+++ hack/ci.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 ./scripts/reset-kind.sh
 export KUBECONFIG="$(kind get kubeconfig-path --name="1")"
+
+# Build the code
 go build
 
 # Fake the secrets from init.yaml
-
 mkdir -p ~/Downloads
 mkdir -p ~/.docker/ 
 touch ~/Downloads/secret-access-key
-touch ~/Downloads/ofc-bootstrap-test.2018-12-23.private-key.pem
+touch ~/Downloads/private-key.pem
 touch ~/.docker/config.json
 
+# Run end to end
 ./ofc-bootstrap -yaml example.init.yaml
