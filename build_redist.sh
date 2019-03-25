2ab6f7ba8657699866e0a8bccd6a87f38e07695e
Alex Ellis <alexellis2@gmail.com>
2019-03-25 19:04:10 +0000

    Update dist path
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- build_redist.sh
+++ build_redist.sh
@@ -10,7 +10,7 @@ mkdir -p bin/
 
 docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t openfaas/ofc-bootstrap:$eTAG . -f Dockerfile.redist && \
  docker create --name ofc-bootstrap openfaas/ofc-bootstrap:$eTAG && \
- docker cp ofc-bootstrap:/root/ofc-bootstrap bin/. && \
- docker cp ofc-bootstrap:/root/ofc-bootstrap-darwin bin/. && \
- docker cp ofc-bootstrap:/root/ofc-bootstrap.exe bin/. && \
+ docker cp ofc-bootstrap:/root/ofc-bootstrap bin/ && \
+ docker cp ofc-bootstrap:/root/ofc-bootstrap-darwin bin/ && \
+ docker cp ofc-bootstrap:/root/ofc-bootstrap.exe bin/ && \
  docker rm -f ofc-bootstrap
