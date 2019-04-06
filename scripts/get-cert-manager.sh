e7dfd44f1b0ffd281f6c92d48d92a0c79a7b1f6e
Vivek Singh <vivekkmr45@yahoo.in>
2019-04-07 03:44:12 +0530

    Add sleep after get cert manager
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- scripts/get-cert-manager.sh
+++ scripts/get-cert-manager.sh
@@ -3,3 +3,5 @@
 # cert-manager is ready for CRD objects when this condition is "True"
 kubectl rollout status cert/cert-manager-webhook-webhook-tls -n  cert-manager --timeout=10m
 kubectl rollout status deploy/cert-manager-webhook -n cert-manager --timeout=10m
+
+sleep 2
