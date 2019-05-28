8ad8c13ad405e0bfb07d2ac674adfddd7650f90d
Chris Kolenko <chris@webcanvas.com.au>
2019-05-28 11:14:46 +1000

    Fix perms with kubeseal
    * Run chmod on separate line to avoid issues with spaces
    
    Signed-off-by: Chris Kolenko <chris@webcanvas.com.au>

--- scripts/export-sealed-secret-pubcert.sh
+++ scripts/export-sealed-secret-pubcert.sh
@@ -10,7 +10,7 @@ then
 #    release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
     echo "SealedSecrets release: $release"
 
-    curl -sLSf https://github.com/bitnami/sealed-secrets/releases/download/$release/kubeseal-$GOOS-$GOARCH > kubeseal && \
+    curl -sLSf https://github.com/bitnami/sealed-secrets/releases/download/$release/kubeseal-$GOOS-$GOARCH > kubeseal
     chmod +x kubeseal
 fi
 
