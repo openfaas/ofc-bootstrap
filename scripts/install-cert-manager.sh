fd822ada3ccc0f06d3a5a1e3f5750e7607975b24
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-09-06 11:55:27 +0100

    Bump up cert-manager helm version
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- scripts/install-cert-manager.sh
+++ scripts/install-cert-manager.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+# Install the CustomResourceDefinition resources separately
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.10/deploy/manifests/00-crds.yaml
 
-kubectl apply \
-    -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
 
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
 helm install \
-    --name cert-manager \
-    --namespace cert-manager \
-    --version v0.6.6 \
-    stable/cert-manager
+  --name cert-manager \
+  --namespace cert-manager \
+  --version v0.10.0 \
+  jetstack/cert-manager
