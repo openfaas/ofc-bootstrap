f13d71467c1e61ec81a2a86c1833bdf9564dfe6c
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-09-06 11:55:27 +0100

    Bump up cert-manager helm version
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- scripts/create-namespaces.sh
+++ scripts/create-namespaces.sh
@@ -2,7 +2,10 @@
 
 kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
 
+# Create the namespace for cert-manager
 kubectl create namespace cert-manager
+
+# Label the cert-manager namespace to disable resource validation
 kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 
 kubectl get namespaces
