372a4661c145286e1665737840f4c295d021648d
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-10-30 16:52:28 +0000

    Update get.sh to support Windows
    Copies approach from k3sup
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- get.sh
+++ get.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 # This script was adapted from https://github.com/openfaas/cli.openfaas.com/blob/master/get.sh
 
-version=$(curl -sI https://github.com/openfaas-incubator/ofc-bootstrap/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
+export OWNER=openfaas-incubator
+export REPO=ofc-bootstrap
+export SUCCESS_CMD="$REPO --version"
+export BINLOCATION="/usr/local/bin"
+
+version=$(curl -sI https://github.com/$OWNER/$REPO/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
 
 if [ ! $version ]; then
-    echo "Failed while attempting to install ofc-bootstrap. Please manually install:"
+    echo "Failed while attempting to install $REPO. Please manually install:"
     echo ""
-    echo "1. Open your web browser and go to https://github.com/openfaas-incubator/ofc-bootstrap/releases"
-    echo "2. Download the latest release for your platform. Call it 'ofc-bootstrap'."
-    echo "3. chmod +x ./ofc-bootstrap"
-    echo "4. mv ./ofc-bootstrap /usr/local/bin"
+    echo "1. Open your web browser and go to https://github.com/$OWNER/$REPO/releases"
+    echo "2. Download the latest release for your platform. Call it '$REPO'."
+    echo "3. chmod +x ./$REPO"
+    echo "4. mv ./$REPO $BINLOCATION"
     exit 1
 fi
 
 hasCli() {
-
-    has=$(which ofc-bootstrap)
-
-    if [ "$?" = "0" ]; then
-        echo
-        echo "You already have the ofc-bootstrap cli!"
-        export n=1
-        sleep $n
-    fi
 
     hasCurl=$(which curl)
     if [ "$?" = "1" ]; then
@@ -36,13 +32,21 @@ getPackage() {
     userid=$(id -u)
 
     suffix=""
+
     case $uname in
     "Darwin")
     suffix="-darwin"
     ;;
+    "MINGW"*)
+    suffix=".exe"
+    BINLOCATION="$HOME/bin"
+    mkdir -p $BINLOCATION
+
+    ;;
     "Linux")
         arch=$(uname -m)
         echo $arch
+
         case $arch in
         "aarch64")
         suffix="-arm64"
@@ -56,17 +60,17 @@ getPackage() {
     ;;
     esac
 
-    targetFile="/tmp/ofc-bootstrap$suffix"
+    targetFile="/tmp/$REPO"
 
     if [ "$userid" != "0" ]; then
-        targetFile="$(pwd)/ofc-bootstrap$suffix"
+        targetFile="$(pwd)/$REPO"
     fi
 
     if [ -e $targetFile ]; then
         rm $targetFile
     fi
 
-    url=https://github.com/openfaas-incubator/ofc-bootstrap/releases/download/$version/ofc-bootstrap$suffix
+    url=https://github.com/$OWNER/$REPO/releases/download/$version/$REPO$suffix
     echo "Downloading package $url as $targetFile"
 
     curl -sSLf $url --output $targetFile
@@ -77,33 +81,47 @@ getPackage() {
 
     echo "Download complete."
 
-        if [ "$userid" != "0" ]; then
+        if [ ! -w "$BINLOCATION" ]; then
 
             echo
-            echo "========================================================="
-            echo "==    As the script was run as a non-root user the     =="
-            echo "==    following commands may need to be run manually   =="
-            echo "========================================================="
+            echo "============================================================"
+            echo "  The script was run as a user who is unable to write"
+            echo "  to $BINLOCATION. To complete the installation the"
+            echo "  following commands may need to be run manually."
+            echo "============================================================"
             echo
-            echo "  sudo cp ofc-bootstrap$suffix /usr/local/bin/ofc-bootstrap"
+            echo "  sudo cp $REPO $BINLOCATION/$REPO"
             echo
 
         else
 
             echo
-            echo "Running as root - Attempting to move ofc-bootstrap to /usr/local/bin"
+            echo "Running with sufficient permissions to attempt to move $REPO to $BINLOCATION"
 
-            mv $targetFile /usr/local/bin/ofc-bootstrap
+            if [ ! -w "$BINLOCATION/$REPO" ] && [ -f "$BINLOCATION/$REPO" ]; then
+
+            echo
+            echo "================================================================"
+            echo "  $BINLOCATION/$REPO already exists and is not writeable"
+            echo "  by the current user.  Please adjust the binary ownership"
+            echo "  or run sh/bash with sudo." 
+            echo "================================================================"
+            echo
+            exit 1
+
+            fi
+
+            mv $targetFile $BINLOCATION/$REPO
 
             if [ "$?" = "0" ]; then
-                echo "New version of ofc-bootstrap installed to /usr/local/bin"
+                echo "New version of $REPO installed to $BINLOCATION"
             fi
 
             if [ -e $targetFile ]; then
                 rm $targetFile
             fi
 
-           ofc-bootstrap -version
+           ${SUCCESS_CMD}
         fi
     fi
 }
