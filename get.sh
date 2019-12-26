280473908c74a26a4f6764cc111c302eb5e1bccf
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-12-26 10:53:30 +0000

    Update version command in get.sh
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- get.sh
+++ get.sh
@@ -9,7 +9,7 @@ export VERIFY_CHECKSUM=0
 export ALIAS=""
 export OWNER=openfaas-incubator
 export REPO=ofc-bootstrap
-export SUCCESS_CMD="$REPO --version"
+export SUCCESS_CMD="$REPO version"
 export BINLOCATION="/usr/local/bin"
 
 ###############################
