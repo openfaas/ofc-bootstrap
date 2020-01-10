5983c08d589d636f8b5aae5334d13e0144564f33
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-01-10 15:30:44 +0000

    Add merge support for YAML files in apply command
    This patch allows multiple override files to be specified that
    will be merged into each parameter given, so you can use a base
    file and then a series of overrides if desired.
    
    Tested with unit tests. Inspiration for the feature comes from
    @csakshaug who proposed the idea and found the merge library.
    
    The merge library is used in Docker, K8s and other high-profile
    projects.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ cmd/merge_test.go
@@ -0,0 +1,93 @@
+// Copyright (c) OpenFaaS Author(s) 2019. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package cmd
+
+import (
+	"testing"
+
+	"github.com/openfaas-incubator/ofc-bootstrap/pkg/types"
+)
+
+func Test_mergePlans_Empty(t *testing.T) {
+
+	_, err := mergePlans([]types.Plan{})
+
+	if err == nil {
+		t.Errorf("Expected an error for no plans")
+		t.Fail()
+	}
+	want := "at least one plan is required"
+	if err.Error() != want {
+		t.Errorf("no plans error want: %s, got: %s", want, err.Error())
+		t.Fail()
+	}
+}
+
+func Test_mergePlans_OnlyOneItem(t *testing.T) {
+
+	plan1 := types.Plan{
+		OpenFaaSCloudVersion: "master",
+	}
+
+	planOut, err := mergePlans([]types.Plan{plan1})
+
+	if err != nil {
+		t.Errorf("Got error for a single plan, expected no error: %s", err.Error())
+		t.Fail()
+	}
+
+	if plan1.OpenFaaSCloudVersion != planOut.OpenFaaSCloudVersion {
+		t.Errorf("OpenFaaSCloudVersion want: %s, but got: %s", plan1.OpenFaaSCloudVersion, planOut.OpenFaaSCloudVersion)
+	}
+}
+
+func Test_mergePlans_MergeEmptyItemsFromBoth(t *testing.T) {
+
+	plan1 := types.Plan{
+		OpenFaaSCloudVersion: "master",
+	}
+
+	plan2 := types.Plan{
+		CustomersURL: "https://127.0.0.1:8443/customers",
+	}
+
+	planOut, err := mergePlans([]types.Plan{plan1, plan2})
+
+	if err != nil {
+		t.Errorf("Got error, expected no error: %s", err.Error())
+		t.Fail()
+	}
+
+	if planOut.OpenFaaSCloudVersion != plan1.OpenFaaSCloudVersion {
+		t.Errorf("OpenFaaSCloudVersion want: %s, but got: %s", plan1.OpenFaaSCloudVersion, planOut.OpenFaaSCloudVersion)
+	}
+
+	if planOut.CustomersURL != plan2.CustomersURL {
+		t.Errorf("CustomersURL want: %s, but got: %s", plan2.CustomersURL, planOut.CustomersURL)
+	}
+}
+
+func Test_mergePlans_PlanValuesOverwriteAccordingToOrder(t *testing.T) {
+
+	plan1 := types.Plan{
+		OpenFaaSCloudVersion: "0.12.0",
+	}
+
+	plan2 := types.Plan{
+		OpenFaaSCloudVersion: "0.11.0",
+	}
+
+	planOut, err := mergePlans([]types.Plan{plan1, plan2})
+
+	if err != nil {
+		t.Errorf("Got error, expected no error: %s", err.Error())
+		t.Fail()
+	}
+
+	wantVer := plan2.OpenFaaSCloudVersion
+	if planOut.OpenFaaSCloudVersion != wantVer {
+		t.Errorf("OpenFaaSCloudVersion want: %s, but got: %s", wantVer, planOut.OpenFaaSCloudVersion)
+	}
+
+}
