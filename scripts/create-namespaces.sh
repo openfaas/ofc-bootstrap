09ab5e0e0f5b396da8c39e271197f9aa4819ce3a
Alistair Hey <alistair@heyal.co.uk>
2020-02-25 15:11:34 +0000

    Bump cert-manager to 0.13.0
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- scripts/create-namespaces.sh
+++ scripts/create-namespaces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the CustomResourceDefinition resources separately
-kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
 
