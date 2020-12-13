86eca1e25911d38759a456e8de538e6695eb2d6c
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-12-13 21:28:53 +0000

    Add wait to the various apps installed
    This was causing a sequencing issue, the prior behaviour was
    to install and wait.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- scripts/install-sealedsecrets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-helm upgrade --install \
- --namespace kube-system ofc-sealedsecrets stable/sealed-secrets \
-  --wait
-
-kubectl rollout status deploy/ofc-sealedsecrets-sealed-secrets -n kube-system --timeout 5m
